9dee5d2315554dd72f8fa2d8f3ba0d731f8c96e0
Melvin Davis <melvin.normandavis@gmail.com>
2026-09-08 15:18:44 +0200

    small cleanup and fixed tests

--- src/main/java/org/example/pensionat_booking/Service/BookingService.java
+++ src/main/java/org/example/pensionat_booking/Service/BookingService.java
@@ -191,10 +191,8 @@ public class BookingService {
 
     public void removeBooking(Long bookingID) {
         if (!bookingRepo.existsById(bookingID)) {
-            System.out.println("ROFL");
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Bokningen hittades ej.");
         }
-        System.out.println("LOLOLOLOL");
         bookingRepo.deleteById(bookingID);
     }
 
